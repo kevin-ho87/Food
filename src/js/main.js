79b275424289b93f03a96d0188a2938e32ea204c
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-11-15 20:05:27 +1100

    Updates
    Add images, carousel buttons and dots, css triangles.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 $('.carousel').slick({
 	dots: true,
+	dotsClass: 'carousel-dots',
 	arrows: true,
-	prevArrow: '<button type="button" class="slick-prev carousel__ctrl carousel__ctrl_prev">&lt;</button>',
-	nextArrow: '<button type="button" class="slick-next carousel__ctrl carousel__ctrl_next">&gt;</button>',
+	prevArrow: '<button type="button" class="slick-prev carousel__ctrl carousel__ctrl_prev"><span class="triangle triangle_left"></span></button>',
+	nextArrow: '<button type="button" class="slick-next carousel__ctrl carousel__ctrl_next"><span class="triangle triangle_right"></span></button>',
 	slidesToShow: 3,
-	slidesToScroll: 3,
+	slidesToScroll: 1,
 	responsive: [
 		{
 			breakpoint: 640,
